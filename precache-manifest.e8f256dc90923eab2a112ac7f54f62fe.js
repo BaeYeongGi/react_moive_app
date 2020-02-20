2f31209dab037a09b369b017150b3d519fc18db5
BaeYeongGi <byg5913@gmail.com>
2020-02-20 15:32:40 +0900

    Updates

---
+++ precache-manifest.e8f256dc90923eab2a112ac7f54f62fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2ed887fd644ba37ed40037e9f26630",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "741093c809f99a9be7a0",
+    "url": "/react_movie_app/static/css/main.2d4c3187.chunk.css"
+  },
+  {
+    "revision": "22809dd729fc6b028285",
+    "url": "/react_movie_app/static/js/2.13691204.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_movie_app/static/js/2.13691204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "741093c809f99a9be7a0",
+    "url": "/react_movie_app/static/js/main.c8512e00.chunk.js"
+  },
+  {
+    "revision": "e27ae280e739db474aba",
+    "url": "/react_movie_app/static/js/runtime-main.bb9d603b.js"
+  }
+]);
